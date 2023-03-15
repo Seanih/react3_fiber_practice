ae49f4ec911d802545343de9611a009d2840ce02
Sean Anih <sean.anih@gmail.com>
2023-03-15 17:31:19 -0400

    added Robot gltfjsx component & styling; ready for animation

--- src/App.js
+++ src/App.js
@@ -1,29 +1,100 @@
-import { RoundedBox } from '@react-three/drei';
+import {
+	RoundedBox,
+	ScrollControls,
+	Scroll,
+	Environment,
+} from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { useRef } from 'react';
+import { Robot } from './components/Robot';
 
 function App() {
-	const test = useRef();
-
-	// animations
-	useFrame((state, delta) => {
-		// console.log(test);
-
-		test.current.rotation.y += delta;
-	});
-
 	return (
 		<>
 			<color attach={'background'} args={['#333333']} />
-			<ambientLight intensity={0.1} />
-			<RoundedBox
-				args={[1, 1, 1]} // Width, height, depth. Default is [1, 1, 1]
-				radius={0.05} // Radius of the rounded corners. Default is 0.05
-				smoothness={4} // The number of curve segments. Default is 4
-				ref={test}
-			>
-				<meshPhongMaterial color='blue' wireframe/>
-			</RoundedBox>
+			<ambientLight intensity={0.2} />
+			<spotLight
+				position={[0, 25, 0]}
+				angle={1.3}
+				penumbra={1}
+				castShadow
+				intensity={2}
+				shadow-bias={-0.0001}
+			/>
+			{/* <Environment background preset='warehouse' /> */}
+			<ScrollControls pages={3} damping={0.1}>
+				{/* Canvas contents in here will *not* scroll, but receive useScroll! */}
+
+				<Robot scale={0.8} />
+				<Scroll>{/* Canvas contents in here will scroll along */}</Scroll>
+				<Scroll html style={{ width: '100%' }}>
+					{/* DOM contents in here will scroll along */}
+					<h1
+						className='title'
+						style={{
+							color: '#cdcbca',
+							position: 'absolute',
+							top: `65vh`,
+							left: '50%',
+							fontSize: '13em',
+							transform: `translate(-50%,-50%)`,
+						}}
+					>
+						PHANTOM
+					</h1>
+
+					<div className='row' style={{ position: 'absolute', top: `132vh` }}>
+						<h2>Be a Man of the Future.</h2>
+						<p style={{ maxWidth: '400px' }}>
+							Featuring a sleek, metallic design inspired by advanced
+							technology, this aftershave bottle is as stylish as it is
+							functional. But it's not just a pretty face - inside, you'll find
+							a nourishing and protective aftershave formula that will leave
+							your skin feeling refreshed and hydrated.
+						</p>
+						<button>Read more</button>
+					</div>
+
+					<div className='row' style={{ position: 'absolute', top: `230vh` }}>
+						<div
+							className='col'
+							style={{ position: 'absolute', right: `40px`, width: '540px' }}
+						>
+							<h2 style={{ maxWidth: '440px' }}>Tech-Savvy Side</h2>
+							<p style={{ maxWidth: '440px' }}>
+								Featuring a sleek, metallic design inspired by advanced
+								technology, this aftershave bottle is as stylish as it is
+								functional. But it's not just a pretty face - inside, you'll
+								find a nourishing and protective aftershave formula that will
+								leave your skin feeling refreshed and hydrated.
+							</p>
+							<button>Read more</button>
+						</div>
+					</div>
+
+					<h2
+						style={{
+							position: 'absolute',
+							top: '350vh',
+							left: '50%',
+							transform: `translate(-50%,-50%)`,
+						}}
+					>
+						Cutting-Edge of Grooming
+					</h2>
+
+					<button
+						style={{
+							position: 'absolute',
+							top: `590vh`,
+							left: '50%',
+							transform: `translate(-50%,-50%)`,
+						}}
+					>
+						Buy now
+					</button>
+				</Scroll>
+			</ScrollControls>
 		</>
 	);
 }

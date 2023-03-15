61b0a34f9c92f694da3f8c9aa45fcb288bf2a298
Sean Anih <sean.anih@gmail.com>
2023-03-14 23:10:06 -0400

    initial commit; dependencies installed and scene is displaying

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { RoundedBox } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const test = useRef();
+
+	// animations
+	useFrame((state, delta) => {
+		// console.log(test);
+
+		test.current.rotation.y += delta;
+	});
+
+	return (
+		<>
+			<color attach={'background'} args={['#333333']} />
+			<ambientLight intensity={0.1} />
+			<RoundedBox
+				args={[1, 1, 1]} // Width, height, depth. Default is [1, 1, 1]
+				radius={0.05} // Radius of the rounded corners. Default is 0.05
+				smoothness={4} // The number of curve segments. Default is 4
+				ref={test}
+			>
+				<meshPhongMaterial color='blue' wireframe/>
+			</RoundedBox>
+		</>
+	);
 }
 
 export default App;
